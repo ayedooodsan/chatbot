b8511e8ba5fe3631d2cce30c9b7101abe6e4d6d4
aditioKLB <aditio.pangestu@klb.co.id>
2019-04-02 10:26:59 +0700

    use style jsx instead of styled componen

--- components/AuthFields/styles.js
+++ components/AuthFields/styles.js
@@ -1,7 +1,19 @@
-export default () => (
-  <style jsx>{`
+import PropTypes from 'prop-types';
+
+const Style = ({ global }) => (
+  <style jsx global={global}>{`
     p {
       color: red;
     }
   `}</style>
 );
+
+Style.defaultProps = {
+  global: false
+};
+
+Style.propTypes = {
+  global: PropTypes.bool
+};
+
+export default Style;
