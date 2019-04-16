ee07d774ffb2e0c282396cbb00883b2c2446a6ca
aditioKLB <aditio.pangestu@klb.co.id>
2019-04-16 16:06:31 +0700

    refactor dialog product

---
+++ components/Dialog/DialogProduct/store.js
@@ -0,0 +1,55 @@
+import { graphql, compose } from 'react-apollo';
+import dialogGql from './dialog.gql';
+import deleteDialogGql from './deleteDialog.gql';
+import updateDialogGql from './updateDialog.gql';
+
+const withDialog = graphql(dialogGql, {
+  name: 'dialog',
+  options: props => ({
+    variables: {
+      id: props.dialogId
+    }
+  }),
+  props: ({ dialog: { loading, dialog, error } }) => {
+    if (error) {
+      return {
+        loading,
+        dialog: {}
+      };
+    }
+    return {
+      loading,
+      dialog,
+      error
+    };
+  }
+});
+
+const withUpdateDialog = graphql(updateDialogGql, {
+  name: 'updateDialog',
+  props: ({ updateDialog }) => ({
+    updateDialog: ({ id }) =>
+      updateDialog({
+        variables: { id },
+        refetchQueries: ['getMyDialogs', 'dialog']
+      })
+  })
+});
+
+const withDeleteDialog = graphql(deleteDialogGql, {
+  name: 'deleteDialog',
+  props: ({ deleteDialog }) => ({
+    deleteDialog: ({ id }) =>
+      deleteDialog({
+        variables: { id },
+        refetchQueries: ['getMyDialogs']
+      })
+  })
+});
+
+export default comp =>
+  compose(
+    withDialog,
+    withUpdateDialog,
+    withDeleteDialog
+  )(comp);
