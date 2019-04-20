18bb34876e9d5fd63ea73a5ac5aab2819338dc0c
aditioKLB <aditio.pangestu@klb.co.id>
2019-04-20 09:38:55 +0700

    create switch project

---
+++ components/Project/index.js
@@ -0,0 +1,164 @@
+import React, { useState, useRef, useEffect } from 'react';
+import classNames from 'classnames';
+import withStyles from '@material-ui/core/styles/withStyles';
+import PropTypes from 'prop-types';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Popper from '@material-ui/core/Popper';
+import Grow from '@material-ui/core/Grow';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import Paper from '@material-ui/core/Paper';
+import Avatar from '@material-ui/core/Avatar';
+import RootRef from '@material-ui/core/RootRef';
+import Button from '@material-ui/core/Button';
+import CreateProductDialog from '../common/CreateProductDialog';
+import { Link } from '../../routes';
+import connect from './store';
+import style from './style';
+import redirect from '../../libraries/redirect';
+
+const Project = props => {
+  const { projectId, myProjects, classes, collapse, createProject } = props;
+  const rootRef = useRef(null);
+  const activeProject = myProjects.find(
+    myProject => myProject.id === projectId
+  ) || { title: '' };
+  const getInitialProject = title =>
+    title.split(' ').reduce((initial, word) => initial + word.charAt(0), '');
+  const [open, setOpen] = useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  useEffect(() => {
+    setOpen(false);
+  }, [projectId]);
+  const [openCreateDialog, setOpenCreateDialog] = useState(false);
+  const createItem = title => {
+    createProject({ title }).then(response => {
+      setOpenCreateDialog(false);
+      const { id } = response.data.createProject;
+      redirect({}, `/${id}/dialog`);
+    });
+  };
+  return (
+    <RootRef rootRef={rootRef}>
+      <React.Fragment>
+        <List>
+          <ListItem
+            button
+            onClick={() => {
+              setOpen(!open);
+            }}
+            className={classes.listItem}
+          >
+            <ListItemIcon className={classes.listItemIcon}>
+              <Avatar className={classes.avatar}>
+                {getInitialProject(activeProject.title)}
+              </Avatar>
+            </ListItemIcon>
+            <ListItemText
+              primary="Project"
+              secondary={activeProject.title}
+              className={classes.listItemText}
+              primaryTypographyProps={{
+                className: classNames(classes.listItemTextTyp, {
+                  [classes.listItemTextTypClose]: !collapse
+                })
+              }}
+              secondaryTypographyProps={{
+                className: classNames(
+                  classes.listItemTextTyp,
+                  classes.listItemTextTypSecondary,
+                  {
+                    [classes.listItemTextTypClose]: !collapse
+                  }
+                )
+              }}
+            />
+          </ListItem>
+        </List>
+        <Popper
+          open={open}
+          anchorEl={rootRef.current}
+          transition
+          placement="right-start"
+        >
+          {({ TransitionProps }) => (
+            <Grow {...TransitionProps}>
+              <Paper className={classes.menuContainer}>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <React.Fragment>
+                    <Typography
+                      variant="overline"
+                      className={classes.menuTitle}
+                    >
+                      Switch Project
+                    </Typography>
+                    <Divider />
+                    <List component="nav" dense>
+                      {myProjects.map(myProject => (
+                        <Link
+                          key={myProject.id}
+                          route={`/${myProject.id}/dialog`}
+                        >
+                          <ListItem button>
+                            <ListItemIcon className={classes.listItemIcon}>
+                              <Avatar className={classes.avatar}>
+                                {getInitialProject(myProject.title)}
+                              </Avatar>
+                            </ListItemIcon>
+                            <ListItemText primary={myProject.title} />
+                          </ListItem>
+                        </Link>
+                      ))}
+                    </List>
+                    <Divider />
+                    <Button
+                      fullWidth
+                      size="small"
+                      color="primary"
+                      onClick={() => {
+                        setOpen(false);
+                        setOpenCreateDialog(true);
+                      }}
+                      className={classes.button}
+                    >
+                      Add Project
+                    </Button>
+                  </React.Fragment>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+        <CreateProductDialog
+          placeholder="Project Name"
+          message="Add new project"
+          open={openCreateDialog}
+          handleClose={() => {
+            setOpenCreateDialog(false);
+          }}
+          handleConfirm={createItem}
+        />
+      </React.Fragment>
+    </RootRef>
+  );
+};
+
+Project.defaultProps = {
+  myProjects: []
+};
+
+Project.propTypes = {
+  projectId: PropTypes.string.isRequired,
+  classes: PropTypes.object.isRequired,
+  collapse: PropTypes.bool.isRequired,
+  createProject: PropTypes.func.isRequired,
+  myProjects: PropTypes.array
+};
+
+export default withStyles(style)(connect(Project));
