aa60ecbdea0b0337ee9ffb81013c7c211fa86abf
aditioKLB <aditio.pangestu@klb.co.id>
2019-05-17 10:14:20 +0700

    update dumb store

--- components/Analytic/DialogAnalytics/storeDumb.js
+++ components/Analytic/DialogAnalytics/storeDumb.js
@@ -5,7 +5,7 @@ const storeDumb = [
     responseMessage:
       'Hai, perkenalkan aku Riska, Asisten Virtual KALBE Store. Silakan pilih atau ketikkan informasi yang ingin Anda ketahui',
     requestTime: '2019-03-29 14:02:41',
-    responseTime: '2019-03-29 14:02:43',
+    responseTime: '2019-03-29 14:02:41',
     groupid: '16818306'
   },
   {
@@ -31,7 +31,7 @@ const storeDumb = [
     responseMessage:
       'Maaf Riska belum mengerti maksud anda karena masih dalam tahap belajar, silakan ulangi lagi pertanyaan Anda atau Tekan Tombol dibawah ini untuk melanjutkan Chat dengan Staff Customer Service kami',
     requestTime: '2019-03-29 14:04:55',
-    responseTime: '2019-03-29 14:04:57',
+    responseTime: '2019-03-29 14:04:55',
     groupid: '17923507'
   },
   {
@@ -40,7 +40,7 @@ const storeDumb = [
     responseMessage:
       'Untuk informasi lebih lanjut silahkan Klik salah satu link dibawah ini untuk menghubungi Customer Service :',
     requestTime: '2019-03-29 14:05:08',
-    responseTime: '2019-03-29 14:05:010',
+    responseTime: '2019-03-29 14:05:08',
     groupid: '17923507'
   },
   {
@@ -50,23 +50,23 @@ const storeDumb = [
       'Silakan pilih keluhan yang anda ingin konsultasikan dengan Ahli Gizi kami :',
     requestTime: '2019-03-29 14:10:18',
     responseTime: '2019-03-29 14:10:19',
-    groupid: '17923508'
+    groupid: '17923507'
   },
   {
     from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
     message: 'Ngeces',
     responseMessage: 'Berapa Usia Adik sekarang?',
     requestTime: '2019-03-29 14:17:07',
-    responseTime: '2019-03-29 14:17:09',
-    groupid: '17923508'
+    responseTime: '2019-03-29 14:17:07',
+    groupid: '17923507'
   },
   {
     from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
     message: '4 th',
     responseMessage: 'Cairan yang keluar sedikit atau banyak?',
     requestTime: '2019-03-29 14:17:11',
-    responseTime: '2019-03-29 14:17:12',
-    groupid: '17923508'
+    responseTime: '2019-03-29 14:17:11',
+    groupid: '17923507'
   },
   {
     from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
@@ -74,7 +74,7 @@ const storeDumb = [
     responseMessage: 'Apakah saat ini adik sedang sakit?',
     requestTime: '2019-03-29 14:17:16',
     responseTime: '2019-03-29 14:17:17',
-    groupid: '17923508'
+    groupid: '17923507'
   },
   {
     from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
@@ -82,8 +82,8 @@ const storeDumb = [
     responseMessage:
       'Hal ini WAJAR, karena kontrol menelan ludah adik saat ini belum baik namun dengan bertambah usia ini akan hilang jika otot syaraf mulutnya  bertambah baik  \nCobalah untuk Mengajak adik berbicara untuk melatih otot-otot mulutnya',
     requestTime: '2019-03-29 14:17:21',
-    responseTime: '2019-03-29 14:17:22',
-    groupid: '17923508'
+    responseTime: '2019-03-29 14:17:21',
+    groupid: '17923507'
   },
   {
     from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
@@ -92,7 +92,7 @@ const storeDumb = [
       'Untuk cara pembayaran di kalbestore, bisa menggunakan methode pembayaran sbb :\nCredit Card\nTransfer Virtual Account (ATM, M-Banking, Permata VA)\nKredivo (iPay88)\nBCA Virtual Account (ATM, m-BCA, KlikBCA)\n \nUntuk Informasi cara pembayaran bisa buka link berikut: https://www.kalbestore.com/how-to-pay/',
     requestTime: '2019-03-29 14:17:25',
     responseTime: '2019-03-29 14:17:26',
-    groupid: '17923509'
+    groupid: '17923507'
   },
   {
     from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
@@ -101,7 +101,7 @@ const storeDumb = [
     responseMessage: 'Kami menemukan produk yang Anda cari!',
     requestTime: '2019-03-29 16:28:50',
     responseTime: '2019-03-29 16:31:31',
-    groupid: '17923510'
+    groupid: '17923507'
   },
   {
     from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
@@ -109,7 +109,7 @@ const storeDumb = [
     responseMessage: 'Kami menemukan produk yang Anda cari!',
     requestTime: '2019-03-29 16:30:58',
     responseTime: '2019-03-29 16:31:31',
-    groupid: '17923510'
+    groupid: '17923507'
   },
   {
     from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
@@ -117,7 +117,7 @@ const storeDumb = [
     responseMessage: 'Kami menemukan produk yang Anda cari!',
     requestTime: '2019-03-29 16:31:27',
     responseTime: '2019-03-29 16:31:31',
-    groupid: '17923511'
+    groupid: '17923507'
   },
   {
     from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
@@ -125,7 +125,7 @@ const storeDumb = [
     responseMessage: 'Kami menemukan produk yang Anda cari!',
     requestTime: '2019-03-29 16:33:28',
     responseTime: '2019-03-29 17:38:20',
-    groupid: '17923512'
+    groupid: '17923507'
   },
   {
     from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
@@ -133,7 +133,7 @@ const storeDumb = [
     responseMessage: 'Kami menemukan produk yang Anda cari!',
     requestTime: '2019-03-29 16:35:25',
     responseTime: '2019-03-29 17:38:20',
-    groupid: '17923513'
+    groupid: '17923507'
   },
   {
     from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
@@ -141,7 +141,120 @@ const storeDumb = [
     responseMessage: 'Kami menemukan produk yang Anda cari!',
     requestTime: '2019-03-29 16:36:50',
     responseTime: '2019-03-29 17:38:20',
-    groupid: '17923514'
+    groupid: '17923507'
+  },
+  {
+    from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
+    message: 'PrenagenÂ EsensisÂ VelvetyÂ ChocolateÂ 180gr',
+    responseMessage: 'Kami menemukan produk yang Anda cari!',
+    requestTime: '2019-03-29 16:38:03',
+    responseTime: '2019-03-29 17:38:20',
+    groupid: '17923507'
+  },
+  {
+    from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
+    message: 'PrenagenÂ EsensisÂ VelvetyÂ ChocolateÂ 180gr',
+    responseMessage: 'Kami menemukan produk yang Anda cari!',
+    requestTime: '2019-03-29 17:32:23',
+    responseTime: '2019-03-29 17:38:20',
+    groupid: '17923507'
+  },
+  {
+    from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
+    message: 'diabetasol',
+    responseMessage: 'Kami menemukan produk yang Anda cari!',
+    requestTime: '2019-03-29 17:33:36',
+    responseTime: '2019-03-29 17:38:20',
+    groupid: '17923507'
+  },
+  {
+    from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
+    message: 'diabetasol',
+    responseMessage: 'Kami menemukan produk yang Anda cari!',
+    requestTime: '2019-03-29 17:35:29',
+    responseTime: '2019-03-29 17:38:20',
+    groupid: '17923507'
+  },
+  {
+    from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
+    message: 'diabetasol',
+    responseMessage: 'Kami menemukan produk yang Anda cari!',
+    requestTime: '2019-03-29 17:36:37',
+    responseTime: '2019-03-29 17:38:20',
+    groupid: '17923507'
+  },
+  {
+    from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
+    message: 'diabetasol',
+    responseMessage: 'Kami menemukan produk yang Anda cari!',
+    requestTime: '2019-03-29 17:38:18',
+    responseTime: '2019-03-29 17:38:20',
+    groupid: '17923507'
+  },
+  {
+    from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
+    message: 'PrenagenÂ EsensisÂ VelvetyÂ ChocolateÂ 180gr',
+    responseMessage: 'Kami menemukan produk yang Anda cari!',
+    requestTime: '2019-03-29 17:38:31',
+    responseTime: '2019-03-29 17:39:47',
+    groupid: '17923507'
+  },
+  {
+    from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
+    message: 'PrenagenÂ EsensisÂ VelvetyÂ ChocolateÂ 180gr',
+    responseMessage: 'Kami menemukan produk yang Anda cari!',
+    requestTime: '2019-03-29 17:39:45',
+    responseTime: '2019-03-29 17:39:47',
+    groupid: '17923507'
+  },
+  {
+    from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
+    message: 'PrenagenÂ EsensisÂ VelvetyÂ ChocolateÂ 360gr',
+    responseMessage: 'Kami menemukan produk yang Anda cari!',
+    requestTime: '2019-03-29 17:40:54',
+    responseTime: '2019-03-29 17:42:06',
+    groupid: '17923507'
+  },
+  {
+    from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
+    message: 'PrenagenÂ EsensisÂ VelvetyÂ ChocolateÂ 180gr',
+    responseMessage: 'Kami menemukan produk yang Anda cari!',
+    requestTime: '2019-03-29 17:42:05',
+    responseTime: '2019-03-29 17:42:06',
+    groupid: '17923507'
+  },
+  {
+    from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
+    message: 'PrenagenÂ EsensisÂ VelvetyÂ ChocolateÂ 360gr',
+    responseMessage: 'Kami menemukan produk yang Anda cari!',
+    requestTime: '2019-03-29 17:42:15',
+    responseTime: '2019-03-29 17:44:43',
+    groupid: '17923507'
+  },
+  {
+    from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
+    message: 'diabetasol',
+    responseMessage: 'Kami menemukan produk yang Anda cari!',
+    requestTime: '2019-03-29 17:44:42',
+    responseTime: '2019-03-29 17:44:43',
+    groupid: '17923507'
+  },
+  {
+    from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
+    message: 'check order',
+    responseMessage: 'Mohon sebutkan nomor order/pesanan anda',
+    requestTime: '2019-03-29 18:21:07',
+    responseTime: '2019-03-29 18:21:12',
+    groupid: '17923507'
+  },
+  {
+    from: 'hfKJ8rkljc1PTaVqG6veKHwzn504GhYq',
+    message: 'halo',
+    responseMessage:
+      'Hai, perkenalkan aku Riska, Asisten Virtual KALBE Store. Silakan pilih atau ketikkan informasi yang ingin Anda ketahui',
+    requestTime: '2019-03-29 18:21:21',
+    responseTime: '2019-03-29 18:21:22',
+    groupid: '17923507'
   }
 ];
 
