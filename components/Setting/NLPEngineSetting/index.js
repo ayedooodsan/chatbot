adf6a414a9d726a477fdbab7c305b264ea50c075
Bayu Wijaya <bayu.wijaya@klb.co.id>
2021-07-02 09:07:59 +0700

    Set General headers text editor json

--- components/Setting/NLPEngineSetting/index.js
+++ components/Setting/NLPEngineSetting/index.js
@@ -19,6 +19,9 @@ import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
 import Switch from '@material-ui/core/Switch';
 import { RemoveRedEye } from '@material-ui/icons';
+import JSONInput from 'react-json-editor-ajrm';
+import locale from 'react-json-editor-ajrm/locale/en';
+
 import ActionConfirmDialog from '../../common/ActionConfirmDialog';
 import UploadFileDialog from '../../common/UploadFileDialog';
 import SimpleProductLayoutProvider from '../../layout/SimpleProductLayoutProvider';
@@ -59,6 +62,7 @@ const NLPEngineSetting = props => {
   );
 
   const [showHidePassword, setShowHidePassword] = useState(true);
+  const [headersValue, setHeadersValue] = useState(null);
 
   const [myIntegration] = myIntegrations;
 
@@ -173,7 +177,8 @@ const NLPEngineSetting = props => {
       gatewayUrl,
       basicAuthUsername,
       basicAuthPassword,
-      timeoutMessage
+      timeoutMessage,
+      generalheaders: headersValue
     };
     updateWebhookProject({
       id: projectId,
@@ -290,7 +295,7 @@ const NLPEngineSetting = props => {
               container
               direction="column"
               spacing={16}
-              style={{ padding: '0px 0px 20px' }}
+              style={{ padding: '0px 0px 50px' }}
             >
               <Grid item>
                 <FormControl fullWidth>
@@ -351,7 +356,7 @@ const NLPEngineSetting = props => {
                   />
                 </FormControl>
               </Grid>
-              <Grid item>
+              {/* <Grid item>
                 <FormControl fullWidth>
                   <TextField
                     value={basicAuthUsername}
@@ -389,7 +394,7 @@ const NLPEngineSetting = props => {
                     type={!showHidePassword ? 'password' : 'text'}
                   />
                 </FormControl>
-              </Grid>
+              </Grid> */}
               <Grid item>
                 <FormControl fullWidth>
                   <TextField
@@ -400,6 +405,35 @@ const NLPEngineSetting = props => {
                     label="Timeout Message"
                     size="small"
                     fullWidth
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item>
+                <FormControl fullWidth>
+                  <Typography
+                    component="div"
+                    style={{
+                      fontSize: 16,
+                      fontWeight: 500,
+                      padding: '20px 0px 5px',
+                      marginBottom: 15
+                    }}
+                  >
+                    Setting Headers JSON
+                  </Typography>
+                  <JSONInput
+                    id="a_unique_id"
+                    theme="dark_vscode_tribute"
+                    placeholder={headersValue}
+                    colors={{
+                      string: '#DAA520'
+                    }}
+                    locale={locale}
+                    height="300px"
+                    width="100%"
+                    onChange={value => {
+                      setHeadersValue(value.jsObject);
+                    }}
                   />
                 </FormControl>
               </Grid>
