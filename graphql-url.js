68fe9d7326fe12f37cb2b72512023d053496f5dc
Agung Nugraha <74890335+ayedooodsan@users.noreply.github.com>
2021-09-10 11:25:37 +0700

    Update graphql-url.js

--- graphql-url.js
+++ graphql-url.js
@@ -1,1 +1,3 @@
-
+export const httpUrl = 'https://console-api.kalina.ai/graphql';
+export const wsUrl = 'wss://console-api.kalina.ai/graphql';
+export const imageUrl = 'https://console-api.kalina.ai/';
